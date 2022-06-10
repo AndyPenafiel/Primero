b68ab0e98141ce18b15f27373d4603dab78c8cd1
chenaoh <cdhenao@gmail.com>
2022-06-10 07:59:18 -0500

    Se agrego el Proyecto

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Document</title>
+	<style>
+		h1{
+			background:#58a4b0 ; /*Color del fondo*/
+			color:#0a0908  ; /*Color del texto*/
+			border: solid 1.5px black ;
+			border-radius: 5px;
+			text-align: center ;
+			font-family: Arial ;
+			box-shadow:5px 5px 10px #348aa7;
+
+		}
+		
+		.menu{
+			background:#d4d700 ;
+			color:#01161e;
+			text-decoration: none;
+			font-family:Arial;
+			padding: 5px ;
+			border-radius: 5px;
+			box-shadow:5px 5px 10px #218380;
+			border: solid 1px black ;
+
+		}
+		
+	</style>
+</head>
+<body bgcolor="#f3ffb6">
+	<h1>Menu Principal</h1>
+	<div align="center"><img src="pagina web.jpg" width="200" height="200"/></div><br>
+	<center><a class="menu" href="lista_estudiantes.php">Estudiantes</a></center><br>
+	<center><a class="menu" href="lista_usuarios.php">Usuarios</a></center>
+</body>
+</html>
